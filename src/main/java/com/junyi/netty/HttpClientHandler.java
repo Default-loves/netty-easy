67a7f14d2034d093e8a86e4f4e1996aab43adce7
Default-loves <2111605130@mail2.gdut.edu.cn>
2020-12-23 18:51:01 +0800

    update

---
+++ src/main/java/com/junyi/netty/HttpClientHandler.java
@@ -0,0 +1,26 @@
+package com.junyi.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * @time: 2020/12/23 17:18
+ * @version: 1.0
+ * @author: junyi Xu
+ * @description:
+ */
+public class HttpClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
---
+++ src/main/java/com/junyi/netty/HttpServerHandler.java
@@ -0,0 +1,28 @@
+package com.junyi.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @time: 2020/12/23 17:12
+ * @version: 1.0
+ * @author: junyi Xu
+ * @description:
+ */
+public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) {
+        String content = String.format("Receive http request, uri: %s, method: %s, content: %s%n", msg.uri(), msg.method(), msg.content().toString(CharsetUtil.UTF_8));
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.wrappedBuffer(content.getBytes()));
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+}
