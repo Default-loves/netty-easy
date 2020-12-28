2388411429f080f575ab6821d66f53e7d7d72433
Default-loves <2111605130@mail2.gdut.edu.cn>
2020-12-28 18:37:25 +0800

    update

---
+++ src/main/java/com/junyi/netty/ByteBufDemo.java
@@ -0,0 +1,83 @@
+package com.junyi.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @time: 2020/12/28 13:44
+ * @version: 1.0
+ * @author: junyi Xu
+ * @description:
+ */
+@Slf4j
+public class ByteBufDemo {
+
+    @Test
+    public void test1() {
+
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(6, 10);
+
+        printByteBufInfo("ByteBufAllocator.buffer(6, 10)", buffer);
+
+        buffer.writeBytes(new byte[]{1, 2});
+
+        printByteBufInfo("write 2 Bytes", buffer);
+
+        buffer.writeInt(100);
+
+        printByteBufInfo("write Int 100", buffer);
+
+        buffer.writeBytes(new byte[]{3, 4, 5});
+
+        printByteBufInfo("write 3 Bytes", buffer);
+
+        int dataLength = buffer.readableBytes();
+        byte[] read = new byte[dataLength];
+        buffer.readBytes(read);
+        printByteBufInfo("readBytes(" + dataLength + ")", buffer);
+
+        try {
+            buffer.writeInt(300);
+        } catch (IndexOutOfBoundsException e) {
+            log.info(e.getMessage());
+        }
+        printByteBufInfo("write Int 300: ", buffer);
+
+        printByteBufInfo("BeforeGetAndSet", buffer);
+
+        System.out.println("getInt(2): " + buffer.getInt(2));
+
+        buffer.setByte(1, 0);
+
+        System.out.println("getByte(1): " + buffer.getByte(1));
+
+        printByteBufInfo("AfterGetAndSet", buffer);
+
+    }
+
+    private static void printByteBufInfo(String step, ByteBuf buffer) {
+
+        System.out.println("------" + step + "-----");
+
+        System.out.println("readerIndex(): " + buffer.readerIndex());
+
+        System.out.println("writerIndex(): " + buffer.writerIndex());
+
+        System.out.println("isReadable(): " + buffer.isReadable());
+
+        System.out.println("isWritable(): " + buffer.isWritable());
+
+        System.out.println("readableBytes(): " + buffer.readableBytes());
+
+        System.out.println("writableBytes(): " + buffer.writableBytes());
+
+        System.out.println("maxWritableBytes(): " + buffer.maxWritableBytes());
+
+        System.out.println("capacity(): " + buffer.capacity());
+
+        System.out.println("maxCapacity(): " + buffer.maxCapacity());
+
+    }
+}
